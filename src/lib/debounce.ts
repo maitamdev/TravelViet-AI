03babb6a2f83828c46b8e87229b6858e6cbcafd0
maitamgk <maitamit062005@gmail.com>
2026-03-04 13:48:20 +0700

    feat(utils): add debounce and throttle helpers

---
+++ src/lib/debounce.ts
@@ -0,0 +1,18 @@
+export function debounce<T extends (...args: any[]) => any>(fn: T, delay: number): T {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return ((...args: any[]) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  }) as T;
+}
+
+export function throttle<T extends (...args: any[]) => any>(fn: T, delay: number): T {
+  let lastCall = 0;
+  return ((...args: any[]) => {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      fn(...args);
+    }
+  }) as T;
+}
