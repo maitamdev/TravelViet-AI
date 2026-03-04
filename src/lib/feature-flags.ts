812e438a18750e55c2fb915faed83c9c4c54e967
maitamgk <maitamit062005@gmail.com>
2026-03-04 13:48:19 +0700

    feat(config): add feature flags configuration

---
+++ src/lib/feature-flags.ts
@@ -0,0 +1,9 @@
+export const FEATURES = {
+  NOTIFICATIONS: true,
+  COLLABORATION: true,
+  BUDGET_TRACKER: true,
+  EXPLORE: true,
+  FAVORITES: true,
+  PROFILE: true,
+  ABOUT: true,
+} as const;
