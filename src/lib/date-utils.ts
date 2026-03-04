823fd3bc5498c953d4124f860dd9d80358932f49
maitamgk <maitamit062005@gmail.com>
2026-03-04 13:48:15 +0700

    feat(utils): add getDaysUntil helper function

---
+++ src/lib/date-utils.ts
@@ -0,0 +1,5 @@
+export function getDaysUntil(dateStr: string): number {
+  const target = new Date(dateStr);
+  const now = new Date();
+  return Math.ceil((target.getTime() - now.getTime()) / (1000 * 60 * 60 * 24));
+}
